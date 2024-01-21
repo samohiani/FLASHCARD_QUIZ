e9743eb81095a3ba0dff464c88f63d375cdf5688
samohiani <ohianisammy2005@gmail.com>
2024-01-21 19:04:13 +0100

    updated timer

--- public/script.js
+++ public/script.js
@@ -91,9 +91,6 @@ const showQuestion = (question) => {
 
 const startTimer = (time) => {
   timer = setInterval(() => {
-    if (time === 3) {
-      playAdudio("countdown.mp3");
-    }
     if (time >= 0) {
       progress(time);
       time--;
