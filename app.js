03ecbfa78b4946263f5046e84dd445cf0ee8b6d1
samohiani <ohianisammy2005@gmail.com>
2024-01-22 23:01:29 +0100

    change port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 const app = express();
-const port = 3000;
+const port = 9000;
 
 mongoose.connect(
   "mongodb+srv://ohianisammy2005:dasakantimuse@cluster0.u6pdi1b.mongodb.net/?retryWrites=true&w=majority",
