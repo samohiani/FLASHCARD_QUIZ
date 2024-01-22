ed5a03b4b5dcdde73a8bd4281a9af962cb666e7a
samohiani <ohianisammy2005@gmail.com>
2024-01-22 22:48:53 +0100

    implementing of mongodb string

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 const port = 3000;
 
-mongoose.connect('mongodb://localhost:27017/quizdb', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(
+  "mongodb+srv://ohianisammy2005:<dasakantimuse>@cluster0.u6pdi1b.mongodb.net/?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true }
+);
 
-const Question = mongoose.model('Question', {
+const Question = mongoose.model("Question", {
   category: String,
   type: String,
   difficulty: String,
@@ -15,41 +18,41 @@ const Question = mongoose.model('Question', {
   incorrect_answers: [String],
 });
 
-app.use(express.static('public'));
-app.set('view engine', 'ejs');
+app.use(express.static("public"));
+app.set("view engine", "ejs");
 
-app.get('/quiz', async (req, res) => {
+app.get("/quiz", async (req, res) => {
   try {
     const questions = await Question.find().limit(10);
     const data = {
       questions: questions,
     };
 
-    res.render('quiz', data);
+    res.render("quiz", data);
   } catch (error) {
-    console.error('Error fetching questions:', error);
-    res.status(500).send('Internal Server Error');
+    console.error("Error fetching questions:", error);
+    res.status(500).send("Internal Server Error");
   }
 });
 
-app.get('/get-questions', async (req, res) => {
+app.get("/get-questions", async (req, res) => {
   try {
-    const selectedCategory = req.query.category || '';
+    const selectedCategory = req.query.category || "";
     const selectedNumQuestions = req.query.amount || 10;
     const questions = await Question.aggregate([
       { $match: { category: selectedCategory } },
-      { $sample: { size: parseInt(selectedNumQuestions) } }
+      { $sample: { size: parseInt(selectedNumQuestions) } },
     ]);
 
     res.json({ success: true, questions });
   } catch (error) {
-    console.error('Error fetching questions:', error);
-    res.json({ success: false, message: 'Error fetching questions' });
+    console.error("Error fetching questions:", error);
+    res.json({ success: false, message: "Error fetching questions" });
   }
 });
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/landingpage.html');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/landingpage.html");
 });
 
 app.listen(port, () => {
